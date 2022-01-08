ef22b7e45e7f591479168cc7240c0d5ceb94ddba
Farukh Saifi <farook1x95@gmail.com>
2022-01-08 20:24:36 +0530

    Create Faker Data with Faker JS

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,37 @@
+import axios from "axios";
 import Link from "next/link";
+import { API_URL } from "../config";
+import EventData from "./types";
+let faker = require("faker");
 
 export default function Banner() {
+  // Create A New random Event
+  const handleClick = async () => {
+    const dummyData = {
+      name: faker.name.firstName(),
+      slug: faker.lorem.slug(),
+      venue: faker.address.streetAddress(),
+      address: `${faker.address.streetAddress()} ${faker.address.city()}, ${faker.address.country()}, ${faker.address.stateAbbr()}(${faker.address.stateAbbr()}), ${faker.address.zipCodeByState()}`,
+
+      performers: faker.company.catchPhrase(),
+      date: faker.date.future(),
+      time: faker.time.recent(),
+      description: faker.lorem.sentence(),
+      image: faker.image.image(),
+    };
+
+    const res = await axios.post(`${API_URL}/api/events`, {
+      data: dummyData,
+    });
+    console.log("res :>> ", res);
+  };
+  // Create bulk Event
+  const bulkFaker = () => {
+    for (let i = 0; i < 10; i++) {
+      handleClick();
+    }
+  };
+
   return (
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
@@ -12,20 +43,20 @@ export default function Banner() {
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
-            <a
-              href="#"
+            <button
+              onClick={handleClick}
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
             >
-              Get started
-            </a>
+              Generate Fake Data
+            </button>
           </div>
           <div className="ml-3 inline-flex rounded-md shadow">
-            <a
-              href="#"
+            <button
+              onClick={bulkFaker}
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
             >
-              Learn more
-            </a>
+              Bulk Faker
+            </button>
           </div>
         </div>
       </div>
